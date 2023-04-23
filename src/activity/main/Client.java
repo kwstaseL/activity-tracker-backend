101ceb36ec3bc063c6947ba02d25fa53949ecf9d
kwstaseL <kostasel19@gmail.com>
2023-04-23 21:05:03 +0300

    Added a segment option for the user

--- src/activity/main/Client.java
+++ src/activity/main/Client.java
@@ -102,7 +102,6 @@ public class Client
 
     public void listenForMessages()
     {
-
         try
         {
             Object routeStats = in.readObject();
@@ -157,12 +156,10 @@ public class Client
         }
     }
 
-    private static void sendRoute()
-    {
+    private static void sendRoute() {
         Scanner scanner = new Scanner(System.in);
 
-        while (true)
-        {
+        while (true) {
             System.out.println("Available files:");
             File directory = new File(Client.directory);
 
@@ -176,8 +173,7 @@ public class Client
             });
 
             // if our directory is empty, there is nothing left to process
-            if (directoryContents == null || directoryContents.length == 0)
-            {
+            if (directoryContents == null || directoryContents.length == 0) {
                 System.out.println("No routes are available for processing!");
                 return;
             }
@@ -187,44 +183,76 @@ public class Client
                 System.out.println(i + ": " + directoryContents[i].getName());
             }
 
+            // list all segments
+            File segmentsDirectory = new File(Client.directory + "/segments");
+            File[] segmentContents = segmentsDirectory.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File file) {
+                    return file.isFile();
+                }
+            });
+            System.out.println("Available segments:");
+            for (int i = 0; i < segmentContents.length; i++) {
+                System.out.println((i + directoryContents.length) + ": " + segmentContents[i].getName());
+            }
 
-            // Prompt user to select a route
+            // Prompt user to select a route or segment
             String input = null;
             Integer choice = null;
 
-            // Acceptable input: all or "all" to send all routes, or anything in the range of 0 to directoryContents.length to send a single route.
-            while (choice == null || choice < 0 || choice >= directoryContents.length)
-            {
-                System.out.println("\nEnter \"all\" to send all routes, or enter a route index (0-" + (directoryContents.length - 1) +") to send a single route.");
-                try
-                {
-                    input = scanner.nextLine();
-                    choice = Integer.valueOf(input);
-                }
-                catch (NumberFormatException e)
-                {
-                    // if the exception was caused by the user typing "all", send all the routes
-                    if (input != null && (input.equalsIgnoreCase("all") || input.equalsIgnoreCase("\"all\"")))
+            // Acceptable input: all or "all" to send all routes/segments, or anything in the range of 0 to the total number
+            // of routes/segments to send a single route/segment.
+            while (choice == null || choice < 0 || choice >= directoryContents.length + segmentContents.length) {
+                System.out.println("\nEnter \"all\" to send all routes/segments, or enter a file index (0-"
+                        + (directoryContents.length + segmentContents.length - 1) +") to send a single route/segment:");
+
+                input = scanner.nextLine();
+
+                if (input.equals("all")) {
+                    // send all routes/segments
+                    for (File file : directoryContents)
                     {
-                        sendAllRoutes(directoryContents);
-                        return;
+                        new Client(file).sendFile();
+                    }
+                    for (File file : segmentContents)
+                    {
+                        new Client(file).sendFile();
+                    }
+                    return;
+                } else {
+                    try {
+                        choice = Integer.parseInt(input);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid input. Please enter a valid file index or \"all\".");
+                        continue;
                     }
 
-                    // else, ignore the invalid input and prompt the user to select a route again
+                    if (choice < 0 || choice >= directoryContents.length + segmentContents.length) {
+                        System.out.println("Invalid input. Please enter a valid file index or \"all\".");
+                        choice = null;
+                        continue;
+                    }
+
+                    File file;
+                    if (choice < directoryContents.length) {
+                        // user selected a route
+                        file = directoryContents[choice];
+                    } else {
+                        // user selected a segment
+                        file = segmentContents[choice - directoryContents.length];
+                    }
+
+                    // send the selected route/segment
+                    Client client = new Client(file);
+                    client.sendFile();
+                    client.listenForMessages();
                 }
             }
-
-            // Select file based on user choice
-            File file = directoryContents[choice];
-
-            Client client = new Client(file);
-            client.sendFile();
-            client.listenForMessages();
         }
     }
 
 
-    //
+
     private static void sendAllRoutes(File[] directoryContents)
     {
         for (File file : directoryContents)
