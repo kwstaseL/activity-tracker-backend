f582755c6ada720c8bf0ebfe09dd377b109d2cb8
kwstaseL <81611844+kwstaseL@users.noreply.github.com>
2023-05-07 12:14:02 +0300

    Added more error handling, added more comments to better reflect on the purpose of each method

--- src/activity/calculations/SegmentStats.java
+++ src/activity/calculations/SegmentStats.java
@@ -2,7 +2,9 @@ package activity.calculations;
 
 import java.io.Serializable;
 
-// SegmentStats: A class that represents the statistics for a specific segment
+/**
+ * This class is used to store the statistics of a segment for a user
+ */
 public class SegmentStats implements Serializable
 {
     // represents the id of the segment this SegmentStats was created for
@@ -12,35 +14,63 @@ public class SegmentStats implements Serializable
     // represents the time it took for the user to complete the segment
     private double time;
 
-    SegmentStats(int segmentID, String fileName)
+    /**
+     * Creates a new instance of SegmentStats.
+     *
+     * @param segmentID the id of the segment
+     * @param fileName the name of the segment file
+     * @throws IllegalArgumentException if segmentID is negative or fileName is null or empty
+     */
+    public SegmentStats(int segmentID, String fileName)
     {
+        if (segmentID < 0)
+        {
+            throw new IllegalArgumentException("segmentID cannot be negative");
+        }
+        if (fileName == null || fileName.isEmpty())
+        {
+            throw new IllegalArgumentException("fileName cannot be null or empty");
+        }
         this.segmentID = segmentID;
         this.fileName = fileName;
         this.time = 0;
     }
 
-    // timeUpdate: Used in the mapping phase to update the time it took for the user to complete the segment
+    /**
+     * Used in the mapping phase to update the time it took for the user to complete the segment.
+     *
+     * @param time the time to update
+     */
     public void timeUpdate(double time)
     {
         this.time += time;
     }
 
+    /**
+     * Returns the time it took for the user to complete the segment.
+     *
+     * @return the time
+     */
     public double getTime()
     {
         return time;
     }
 
-    public int getSegmentID()
-    {
-        return this.segmentID;
-    }
-
+    /**
+     * Returns the name of the segment file.
+     *
+     * @return the file name
+     */
     public String getFileName()
     {
         return fileName;
     }
 
-    // Equals function that only compares the segmentID between two SegmentStats
+    /**
+     *  Custom equals function that compares two SegmentStats objects based on their segmentID.
+     * @param o the object to compare to
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o)
     {
